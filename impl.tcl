f30910e64649735768284854a53257d37da816d5
Orgoutsider <2638552655@qq.com>
2023-11-12 06:02:25 +0800

    save

--- impl.tcl
+++ impl.tcl
@@ -1351,3 +1351,18 @@ dev_map
 pnr 
 report_timing 
 gen_bit_stream -master_configuration_clock_frequency {40M} 
+add_design E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rotate_fram_buf/rotate_fram_buf.idf
+remove_design -force E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/ethernet/ipcore/ref_clock/ref_clock.idf
+remove_design -force -verilog "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/ethernet/rtl/udp_rx_bac.v"
+set_arch -family Logos -device PGL50H -speedgrade -6 -package FBG484
+compile -top_module hdmi_ddr_ov5640_top
+synthesize -ads -selected_syn_tool_opt 2 
+dev_map 
+pnr 
+report_timing 
+gen_bit_stream -master_configuration_clock_frequency {40M} 
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/coor_trans.v"
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/coor_trans_forward.v"
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/coor_trans_reverse.v"
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/cos_table.v"
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/sin_table.v"
