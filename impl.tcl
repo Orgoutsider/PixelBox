f52da45eab32362d6f4a9e79c5f09558c84e509e
Orgoutsider <2638552655@qq.com>
2023-11-11 21:00:07 +0800

    sat

--- impl.tcl
+++ impl.tcl
@@ -1334,3 +1334,12 @@ pnr
 report_timing 
 gen_bit_stream -master_configuration_clock_frequency {40M} 
 add_design E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/ipcore/rom/rom.idf
+add_design "E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/source/rtl/saturation.v"
+set_arch -family Logos -device PGL50H -speedgrade -6 -package FBG484
+compile -top_module hdmi_ddr_ov5640_top
+synthesize -ads -selected_syn_tool_opt 2 
+dev_map 
+pnr 
+report_timing 
+gen_bit_stream -master_configuration_clock_frequency {40M} 
+remove_design -force E:/Verilog/pango_prj/HDMI_DDR3_OV5640_test/ipcore/rom/rom.idf
